917c9fc29f0f00bbc2b6a8063f39a94695f1c8e3
Tom Salter <tomjsalter@protonmail.com>
2022-10-10 21:44:01 +0100

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-// create function called getComputerChoice
 function getComputerChoice() {
-
+  let getNumber = Math.floor(Math.random() * 3);
+  if (getNumber === 0) {
+    return getNumber = "rock";
+  } else if (getNumber === 1) {
+    return getNumber = "paper";
+  } else if (getNumber === 2) {
+    return getNumber = "scissors";
+  }
 }
-
-// generate random number from 1 to 3
-let getNumber = Math.floor(Math.random() * 3);
-
-// assign each number a string: 'rock', 'paper' or 'scissors'
-if (getNumber === 0) {
-    getNumber = 'rock';
-} else if (getNumber === 1) {
-    getNumber = 'paper';
-} else if (getNumber === 2) {
-    getNumber = 'scissors';
-}
-
-// return computer choice
