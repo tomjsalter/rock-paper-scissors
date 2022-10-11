9abc0e061ec0aa551733c7b2ac5c67d79794c173
Tom Salter <tomjsalter@protonmail.com>
2022-10-11 06:54:59 +0100

    Make string returns template literals

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 function getComputerChoice() {
   let getNumber = Math.floor(Math.random() * 3);
-switch (getNumber) {
-  case 0:
-    return getNumber = "rock";
-    break;
-  case 1:
-    return getNumber = "paper";
-    break;
-  case 2:
-    return getNumber = "scissors";
-    break;
-}
+  switch (getNumber) {
+    case 0:
+      return (getNumber = "rock");
+      break;
+    case 1:
+      return (getNumber = "paper");
+      break;
+    case 2:
+      return (getNumber = "scissors");
+      break;
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
   computerSelection = getComputerChoice();
-  playerSelection = prompt('Rock, Paper or Scissors?');
+  playerSelection = prompt("Rock, Paper or Scissors?");
   playerSelection = playerSelection.toLowerCase();
   if (computerSelection === playerSelection) {
-    return "It's a draw!";
+    return `It's a draw. Computer chose: ${computerSelection} Player chose ${playerSelection}`;
   } else if (
-    (computerSelection === "rock" && playerSelection === "scissors") || 
+    (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper")
-    ) {
-    return "Computer wins!";
+  ) {
+    return `Computer wins! ${computerSelection} beats ${playerSelection}`;
   } else {
-    return "Player wins!";
+    return `Player wins! ${playerSelection} beats ${computerSelection}`;
   }
 }
